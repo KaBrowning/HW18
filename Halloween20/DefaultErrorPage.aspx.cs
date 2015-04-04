47e5ec60f6384e0f7a7a72e5a4db4cbbde97a3d1
Kathryn Browning <kbrowni3@my.westga.edu>
2015-04-04 13:33:00 -0400

    Added more comments

--- Halloween20/DefaultErrorPage.aspx.cs
+++ Halloween20/DefaultErrorPage.aspx.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 
+/// <summary>
+/// This class describes the Default Error page
+/// </summary>
+/// <author>Kathryn Browning</author>
+/// <version>April 4, 2015</version>
 public partial class DefaultErrorPage : System.Web.UI.Page
 {
+    /// <summary>
+    /// Handles the Load event of the Page control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, EventArgs e)
     {
 
