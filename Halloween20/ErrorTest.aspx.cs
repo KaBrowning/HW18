a8c3dc8810c176141cdaa6632967655ded4648e3
Kathryn Browning <kbrowni3@my.westga.edu>
2015-04-04 13:28:13 -0400

    Fixed some ReSharper errors and added some comments

--- Halloween20/ErrorTest.aspx.cs
+++ Halloween20/ErrorTest.aspx.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
 
 /// <summary>
-/// 
+/// This class describes the Error Test page
 /// </summary>
+/// <author>Kathryn Browning</author>
+/// <version>April 4, 2015</version>
 public partial class ErrorTest : System.Web.UI.Page
 {
     /// <summary>
